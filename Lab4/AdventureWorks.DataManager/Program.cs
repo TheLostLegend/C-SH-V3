b37b8765830c5ff149d0f81a3d47baf750cdfee7
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-12-15 22:00:29 +0300

    Add files via upload

---
+++ Lab4/AdventureWorks.DataManager/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ConfigManager;
+using AdventureWorks.Models;
+using AdventureWorks.ServiceLayer;
+using System.Data.SqlClient;
+using ToXmlService;
+
+namespace AdventureWorks.DataManager
+{
+    class DataManager
+    {
+        static void Main(string[] args)
+        {
+            
+            string configPath = @"D:\config.xml";
+            StrEnter MainPath= new StrEnter();
+            MainPath = MainPath.GetConnectionString(configPath);
+            string connectionString = MainPath.Input;
+            Console.WriteLine(connectionString);
+            XMLCriteria searchCriteria = new XMLCriteria();
+            searchCriteria.StartId = 43659;
+            searchCriteria.Count = 3;
+
+            ArrayDataXML ordersProvider = new ArrayDataXML(connectionString);
+            var salesOrders = ordersProvider.GetOrders(searchCriteria);
+            ToXml generator = new ToXml(MainPath.Output, "SalesOrders");
+            generator.XMLGenerate(salesOrders, searchCriteria);
+            generator.SaveXsd();
+
+            Console.WriteLine("Подключение закрыто...");
+            Console.ReadKey();
+        }
+    }
+}
