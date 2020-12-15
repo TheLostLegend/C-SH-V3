de20464b90797451cce5309bb14c821b527f8754
TheLostLegend <61868705+TheLostLegend@users.noreply.github.com>
2020-12-15 21:54:59 +0300

    Add files via upload

---
+++ Lab4/AdventureWorks.ServiceLayer/ArrayDataXML.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventureWorks.Models;
+using AdventureWorks.DataAccess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AdventureWorks.ServiceLayer
+{
+    public class ArrayDataXML
+    {
+        private string connectionString;
+        public ArrayDataXML(string connection)
+        {
+            connectionString = connection;
+        }
+        public SearchResult<SalesOrder>[] GetOrders(XMLCriteria searchCriteria)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            SearchResult<SalesOrder>[] searchResults = new SearchResult<SalesOrder>[searchCriteria.Count];
+            ArrayPartXML storedProcedure = new ArrayPartXML(connectionString);
+            int currentId = searchCriteria.StartId;
+            int lastId = searchCriteria.StartId + searchCriteria.Count;
+
+            for (int i = 0; currentId < lastId; i++, currentId++)
+            {
+                searchResults[i] = storedProcedure.ReadOne(currentId);
+            }
+            return searchResults;
+        }
+    }
+}
